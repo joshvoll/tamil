87703596f4779f1bc6745bdebe42efed539e7334
Josue Rodriguez <joshvoll@yahoo.com>
2020-07-11 11:14:14 -0600

    adding node, statement and expression

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/joshvoll/tamil/token"
+
 // Node representation
 type Node interface {
 	TokenLiteral() string
@@ -16,3 +18,41 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// Program definition
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral implements Node
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatement represent the LET statment variable binding
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+// statementNode helper method
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral helper method
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier definition
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+// expresionNode implements Expression
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral implements Node
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
